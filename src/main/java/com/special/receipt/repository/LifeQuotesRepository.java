63f4541f4fef0b008c6f4df8f6ef7a704ecd692e
MyungHwanKim <code.kmh9250@gmail.com>
2023-01-24 11:01:13 +0900

    feat : receipt 주문 결과 명언 추가 기능

---
+++ src/main/java/com/special/receipt/repository/LifeQuotesRepository.java
@@ -0,0 +1,7 @@
+package com.special.receipt.repository;
+
+import com.special.receipt.domain.LifeQuotes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LifeQuotesRepository extends JpaRepository<LifeQuotes, Long> {
+}
