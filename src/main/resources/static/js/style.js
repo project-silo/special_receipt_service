03350cabf728c7935421752f082e5a73a4ccc3d4
MyungHwanKim <code.kmh9250@gmail.com>
2023-01-24 11:01:49 +0900

    feat : receipt 주문 결과 명언 추가 page 및 공유하기 기능 구현

--- src/main/resources/static/js/style.js
+++ src/main/resources/static/js/style.js
@@ -13,3 +13,15 @@ function order() {
         }, 450)
     }, 450);
 }
+
+function copy() {
+    var url = '';
+    var textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    url = window.document.location.href;
+    textarea.value = url;
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert("URL이 복사되었습니다.");
+}
